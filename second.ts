d1d93abacedf159f8169bd0c695770520b56e330
Randy <dhcho1034@gmail.com>
2023-01-11 01:25:06 +0900

    기본문법(~void)

--- second.ts
+++ second.ts
@@ -58,4 +58,35 @@ const second = () => {
     type E = { hello: 'world' } | { zero: 'cho' } // 여러 속성 중 하나만 있으면 됨(union)
     const e1: E = { hello: 'world' } // 가능
     const e2: E = { hello: 'world', zero: 'cho' } // 가능
+
+    type Animal = { breath: true }
+    type Mammalia = Animal & { breed: true } // 상속
+    type Human = Mammalia & { think: true } // 상속
+
+    const randy: Human = { breath: true, breed: true, think: true }
+
+    // type, interface 간에 서로 extends, & 등 가능하다
+    interface AA {
+        breath: true
+    }
+    interface BB extends AA {
+        breed: true
+    }
+    const bb: BB = { breath: true, breed: true }
+
+    // 같은 이름으로 interface 선언할 때마다 합쳐진다
+    // 이 특성으로 남의 라이브러리 고쳐 쓸 수 있음
+    interface CC {
+        talk: () => void
+    }
+    interface CC {
+        eat: () => void
+    }
+    interface CC {
+        shit: () => void
+    }
+    const cc: CC = { talk() {}, eat() {}, shit() {} }
+
+    // naming rule: interface앞에 I, type앞에 T, enum앞에 E 붙이는 경우가 있고 안붙이는 경우가 있음
+    // 요즘 추세는 안붙임
 }
---
+++ third.ts
@@ -0,0 +1,55 @@
+const third = () => {
+    type A1 = string | number // 넓은 타입 (합집합)
+    type A2 = string // 좁은 타입
+    type A3 = string & number // 더 좁은 타입 (교집합) - never: 공집합 / any: 전체 집합
+
+    // 객체는 상세할 수록 더 좁다
+    type A = { name: string } // 넓은 타입
+    type B = { age: number } // 넓은 조입
+    type C = { name: string; age: number } // 좁은 타입
+    type AB = A | B
+    type D = A & B // C랑 같음
+
+    const ab: AB = { name: 'randy' } // 더 넓은 타입에 더 좁은 타입 대입 가능
+    // const c: C = { name: 'randy' } // 더 좁은 타입에 더 넓은 타입 대입 불가능
+
+    // 잉여 속성 검사
+    // const c: C = { name: 'randy', age: 34, married: false } // 더 넓은 타입에 더 좁은 타입 대입했는데도 안됨(잉여 속성 검사)
+    const obj = { name: 'randy', age: 34, married: false }
+    const d: D = obj // 이렇겐 가능
+
+    // void
+    // 함수의 return 값이 void - return 값이 '없다'란 의미
+    // return 값 쓰면 에러남
+    function a(callback: () => void): void {
+        return // return '3' 안됨
+    }
+    // 매개변수, 메서드의 return 값이 void - return 값을 '사용하지 않겠다'는 의미
+    // return 값 써도 됨
+    a(() => {
+        return '3'
+    })
+
+    interface Human {
+        talk: () => void
+    }
+    const human: Human = {
+        talk() {
+            return 'abc'
+        },
+    }
+
+    // const randy = human.talk()  // void가 되어버림
+    // const randy = human.talk() as string // 에러남
+    const randy2 = human.talk() as unknown as string // 의도적으로 바꾸려면
+}
+
+// declare 선언하면 구현부가 없어도 됨 (외부에서 구현되어 있는 함수를 타입선언함)
+declare function forEach(arr: number[], callback: (el: number) => void): void
+
+const target: number[] = []
+// 아래와 같은 두 가지 경우를 모두 허용하기 위해서 return 값을 신경쓰지 않는다.
+forEach([1, 2, 3], el => target.push(el)) // undefined: 에러남 / void: 에러 안남 (return 값을 신경쓰지 않겠다)
+forEach([1, 2, 3], el => {
+    target.push(el)
+})
